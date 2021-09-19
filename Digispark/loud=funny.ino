69f8a8ab9916aaf888ba3bc401af74d186cbeaf8
slashdiv <tbergy44@gmail.com>
2021-09-19 13:50:48 -0500

    Create loud=funny.ino

---
+++ Digispark/loud=funny.ino
@@ -0,0 +1,46 @@
+#include "DigiKeyboard.h"
+
+void setup() {
+  digitalWrite(1,HIGH);
+
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+
+  delay(100);
+
+  DigiKeyboard.print("powershell.exe");
+
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+  delay(500);
+
+  DigiKeyboard.print("Function Set-Speaker($Volume){$wshShell = new-object -com wscript.shell;1..50 | % {$wshShell.SendKeys([char]174)};1..$Volume | % {$wshShell.SendKeys([char]175)}}");
+
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+  delay(10);
+
+  DigiKeyboard.print("Set-Speaker -Volume 50");
+
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+  delay(10);
+
+  //Add YouTube Link here!
+                                                 
+  DigiKeyboard.print("start chrome.exe https://www.youtube.com/watch?v=adtuUeRb4Ts");
+
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+    delay(10);
+
+      DigiKeyboard.sendKeyStroke(KEY_L, MOD_GUI_LEFT);
+
+  digitalWrite(1,LOW);
+}
+
+
+void loop() {
+
+  
+ 
+}
