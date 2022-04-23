32a8eaf58858cec8b4e57cdb87f2bdd9e1f3b152
slash1div <tbergy44@gmail.com>
2022-04-23 13:44:50 -0500

    Update UnfocusOptimizer.lua

--- ROBLOX/autoexecute/unified-scripts/UnfocusOptimizer.lua
+++ ROBLOX/autoexecute/unified-scripts/UnfocusOptimizer.lua
@@ -1,1 +1,7 @@
+--[[
+    Unfocus optimizer
+    Made by gamermanaway on v3rm
+    https://v3rmillion.net/showthread.php?tid=1143864
+--]]
+
 loadstring(game:HttpGet("https://pastebin.com/raw/2MqFBmsU", true))()
