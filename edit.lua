16e52620a4128dc8a800487e184b41031f44dfda
slash1div <tbergy44@gmail.com>
2022-07-23 15:34:03 -0500

    Update edit.lua

--- edit.lua
+++ edit.lua
@@ -511,7 +511,7 @@ function Library:CreateMain()
 	MinMax.TextWrapped = true
 	UICorner_9.Parent = MinMax
 
-	
+
 	--Scripting
 
 	if isGameSupported(getgenv().supportedGameList) then
@@ -556,7 +556,7 @@ function Library:CreateMain()
 		TextButton_2.TextColor3 = Color3.fromRGB(0, 0, 0)
 		TextButton_2.TextSize = 12.000
 		TextButton.MouseButton1Down:connect(function()
-			game:GetService("TeleportService"):Teleport(v, game:GetService("Players").LocalPlayer)
+			game:GetService("TeleportService"):Teleport(tonumber(v), game:GetService("Players").LocalPlayer)
 		end)
 		TextButton_2.MouseButton1Down:connect(function()
 			setclipboard("https://www.roblox.com/games/".. v)
